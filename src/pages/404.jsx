fdb447dc5d05b47689ecccd55f6398599da4d7b9
adrianrivers <adrianlrivers@gmail.com>
2019-07-18 15:45:09 +0200

    Reorder files

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <Image />
+  </Layout>
+);
+
+export default NotFoundPage;
