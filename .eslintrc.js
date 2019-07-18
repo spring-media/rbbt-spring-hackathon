f2f530da2ab9fea4c7778383b38dde701cd93161
adrianrivers <adrianlrivers@gmail.com>
2019-07-18 16:23:52 +0200

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,30 @@
 module.exports = {
   "parser": "babel-eslint",
   "extends": [
-    "airbnb"
+    "airbnb",
+    "prettier",
+    "prettier/react"
   ],
-  "plugins": [
-    "prettier"
-  ]
+  "parserOptions": {
+    "experimentalObjectRestSpread": true,
+    "impliedStrict": true,
+    "classes": true
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+    "jquery": true
+  },
+  "rules": {
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "es5",
+        "semi": true,
+        "singleQuote": true,
+        "printWidth": 120
+      }
+    ],
+  },
+  "plugins": ["prettier"]
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,12 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
 };
